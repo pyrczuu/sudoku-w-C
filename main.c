f530f6a6cbf256f883c02c07681c414ee590c74b
pyrczuu <dydekpyrcz@gmail.com>
2025-04-30 21:03:31 +0200

    zrobione

--- main.c
+++ main.c
@@ -6,27 +6,8 @@
 int main(void) {
     srand ( time(NULL) );
 
-    int k = 20;
-    int size = 16;
-    int **tablica = malloc(size*sizeof(int*));
-    for (int i = 0; i < size; i++) {
-        tablica[i] = malloc(size*sizeof(int));
-    }
-    tablica = sudokuGenerator(size,k);
+    menuScreen();
 
-    // for (int i = 0; i < size; i++) {
-    //     for (int j = 0; j < size; j++) {
-    //         printf("%d ", sudoku[i][j]);
-    //     }
-    //     printf("\n");
-    // }
-
-    printGrid(tablica,size);
-
-    for (int j = 0; j < size; j++) {
-        free(tablica[j]);
-    }
-    free(tablica);
     return 0;
 }
 
