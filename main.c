3df80438060449c690ab8cb993b8189dc6db5f57
pyrczuu <dydekpyrcz@gmail.com>
2025-05-12 22:41:55 +0200

    added sudoku solver using simulated anealing

--- main.c
+++ main.c
@@ -2,12 +2,17 @@
 #include <stdlib.h>
 #include <time.h>
 #include "engine.h"
+#include "solver.h"
 
 int main(void) {
     srand ( time(NULL) );
 
-    menuScreen();
+    //menuScreen();
 
+    int **sudoku = sudokuGenerator(9,20);
+    printGrid(sudoku,9);
+    simulated_anealing(sudoku,9 , 10.0, 0.001, 0.995, 100000 );
+    freeGrid(sudoku,9);
     return 0;
 }
 
